2be53bdffd075b74218a0859a8a10e69638a855f
Tyomay1999 <artyom.bordulanyk@gmail.com>
2020-12-01 01:37:34 +0300

    create component`s files

---
+++ src/Component/Modal/modal.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+const Modal = (props) => {
+    return (
+        <div>
+            <p>Modal</p>
+        </div>
+    )
+}
+
+export default Modal;
---
+++ src/Component/TodoList/todoList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import todoListModule from './todoList.module.css';
+
+const TodoList = (props) => {
+    return (
+        <div>
+            Todo List
+        </div>
+    )
+}
+
+export default TodoList;
