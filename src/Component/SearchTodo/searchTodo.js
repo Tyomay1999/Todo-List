8abe6d8ea2fdcb175c9a3079eb25912c847bdade
Tyomay1999 <artyom.bordulanyk@gmail.com>
2020-12-01 18:13:56 +0300

    modefied-search

--- src/Component/SearchTodo/searchTodo.js
+++ src/Component/SearchTodo/searchTodo.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { deletTodo } from '../Redux/action';
 import searchTodoModule from './searchTodo.module.css';
 
-const SearchTodo = (props) => {
+const SearchTodo = ({todos,setData,data}) => {
     const [searchTodo,setSearchTodo] = useState('')
+    const dispatch = useDispatch()
     return (
         <div className={searchTodoModule.searchImput}>
             <form className={searchTodoModule.form}>
@@ -10,17 +13,29 @@ const SearchTodo = (props) => {
                 <input
                     id={searchTodoModule.search}
                     type="search"
-                    placeholder="     Write Todo name"
+                    placeholder="     Write Todo title"
                     onChange={(e) => { setSearchTodo(e.target.value) }}
                     required
                 />
                 <span className={searchTodoModule.caret}></span>
             </form>
             <div className={searchTodo ? `${searchTodoModule.searchDiv}` : `${searchTodoModule.searchDivNone}`}>
-                <p className={searchTodoModule.searchitem}>todo</p>
-                <p className={searchTodoModule.searchitem}>todo</p>
-                <p className={searchTodoModule.searchitem}>todo</p>
-                <p className={searchTodoModule.searchitem}>todo</p>
+                {todos && todos.map((item,index)=> {
+                    if(item.title === searchTodo){
+                        return(
+                            <div className={searchTodoModule.modalDiv} key={index}>
+                                <p className={searchTodoModule.searchitem}>{searchTodo}</p>
+                                <button  
+                                className={searchTodoModule.modalButtons} 
+                                onClick={() => {
+                                    setData(!data)
+                                    dispatch(deletTodo(item._id))
+                                }}
+                                >Delete</button>
+                            </div>
+                        )
+                    }
+                })}
             </div>
         </div>
     )
--- src/app.js
+++ src/app.js
@@ -1,20 +1,10 @@
 import React from 'react';
-import SearchTodo from './Component/SearchTodo/searchTodo';
 import TodoList from './Component/TodoList/todoList';
-// import axios from 'axios';
-// import { API_URL_GET_CREATE } from './config';
-// axios.get(API_URL_GET_CREATE)
-// .then(data => {
-//     console.log(data)
-//     return data
-// })
-// .then(data => console.log(data))
 
 
 const App = () => {
     return(
         <div>
-            <SearchTodo/>
             <TodoList/>
         </div>
     )
