fc8c52317ab30c5581cfb88b84400e380df1396c
Tyomay1999 <artyom.bordulanyk@gmail.com>
2020-12-01 11:08:44 +0300

    config.js

---
+++ src/config.js
@@ -0,0 +1,2 @@
+export const API_URL_GET_CREATE = 'https://todo.eachbase.com/api/ArtyomBordulanyuk/todos';
+export const API_URL_GET_ID_UPDATE_DELETE = 'https://todo.eachbase.com/api/ArtyomBordulanyuk/todos/:todoId';
